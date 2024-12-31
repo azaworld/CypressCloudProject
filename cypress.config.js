eeadcb9d0ce10b1769739644d95978130c8a4e50
Arifuz Zaman Antor <arifuzzaman.antor@miaki.com.bd>
2024-12-31 13:20:43 +0600

    Cypress cloud project installed successfully and pushed all the specs

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+module.exports = {
+  projectId: "zou9xn", // Your project ID from Cypress Cloud
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    //baseUrl: "http://localhost:3000", // Replace with your application URL
   },
-});
+};
+
